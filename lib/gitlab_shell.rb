3f8a8ade25bb81dbc04565d866686281010f6d4b
François Bobot <francois.bobot@cea.fr>
2013-09-16 11:23:41 +0200

    proof of concept for allowing git-annex

--- lib/gitlab_shell.rb
+++ lib/gitlab_shell.rb
@@ -4,7 +4,7 @@ require 'shellwords'
 require_relative 'gitlab_net'
 
 class GitlabShell
-  attr_accessor :key_id, :repo_name, :git_cmd, :repos_path, :repo_name
+  attr_accessor :key_id, :repo_name, :git_cmd, :repos_path, :repo_name, :git_annex_cmd, :git_annex_args
 
   def initialize
     @key_id = /key-[0-9]+/.match(ARGV.join).to_s
@@ -28,6 +28,16 @@ class GitlabShell
           $logger.warn message
           $stderr.puts "Access denied."
         end
+      elsif @git_cmd == "git-annex-shell"
+        if write_access
+          process_annex_cmd(false)
+        elsif read_access
+          process_annex_cmd(true)
+        else
+          message = "gitlab-shell: Access denied for git annex command <#{@origin_cmd}> by #{log_username}."
+          $logger.warn message
+          $stderr.puts "Access denied."
+        end
       else
         message = "gitlab-shell: Attempt to execute disallowed command <#{@origin_cmd}> by #{log_username}."
         $logger.warn message
@@ -43,7 +53,14 @@ class GitlabShell
   def parse_cmd
     args = Shellwords.shellwords(@origin_cmd)
     @git_cmd = args[0]
-    @repo_name = args[1]
+    if @git_cmd == "git-annex-shell"
+      @git_annex_cmd = args[1]
+      #remove leading ~/
+      @repo_name = args[2].gsub(/~\//,"")
+      @git_annex_args = args[3..-1]
+    else
+      @repo_name = args[1]
+    end
   end
 
   def git_cmds
@@ -56,12 +73,38 @@ class GitlabShell
     exec_cmd(@git_cmd,repo_full_path)
   end
 
+  def process_annex_cmd(read_only)
+    # git-annex restrict itself to read_only command
+    if read_only
+        ENV['GIT_ANNEX_SHELL_READONLY'] = '1'
+    end
+    # limit git-annex-shell to safe command
+    ENV['GIT_ANNEX_SHELL_LIMITED'] = '1'
+    repo_full_path = File.join(repos_path, repo_name)
+    ENV['GIT_ANNEX_SHELL_DIRECTORY'] = repo_full_path
+
+    $logger.info "gitlab-shell: executing git annex command <#{@git_cmd} #{@git_annex_cmd} #{repo_full_path} #{@git_annex_args.join(" ")}> for #{log_username} and (read_only=#{read_only})."
+    exec_annex_cmd(@git_annex_cmd,repo_full_path,@git_annex_args)
+  end
+
   def validate_access
     api.allowed?(@git_cmd, @repo_name, @key_id, '_any')
   end
 
+  def read_access
+    api.allowed?("git-receive-pack", @repo_name, @key_id, '_any')
+  end
+
+  def write_access
+    api.allowed?("git-upload-pack", @repo_name, @key_id, '_any')
+  end
+
   def exec_cmd *args
     Kernel::exec *args
+  end
+
+  def exec_annex_cmd(cmd,repo,args)
+    Kernel::exec("git-annex-shell",cmd,repo,*args)
   end
 
   def api
